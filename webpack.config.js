35f79f45a3b96c759d9fa2faa5c7fe47cc905aae
Francis Gulotta <wizard@roborooter.com>
2019-01-02 22:45:50 -0500

    webpack and play videos from the url

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  target: 'web',
+  devtool: 'source-map',
+  entry: [
+    './lib/index.js'
+  ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'IPFS Videostream example',
+      template: './lib/index.html'
+    })
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'client.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000
+  }
+}
